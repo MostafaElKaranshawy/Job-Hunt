11833bad3f8af3efab0b5d7c5731d379950acd72
Mostafa Elkaranshawy <mostafaelkaranshawy0@gmail.com>
2024-12-04 07:15:13 +0200

    add Log Out

--- frontend/src/pages/profilePage/profilePage.jsx
+++ frontend/src/pages/profilePage/profilePage.jsx
@@ -1,17 +1,37 @@
-import React, {} from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import Header from '../../components/header/header';
 import ProfileSectionNavigator from "../../components/profileSections/profileSectionsNavigator/profileSectionsNavigator";
-
+import ConfirmationBox from "../../components/confirmationBox/confirmationBox.";
 import "./profilePage.css";
 
 export default function ProfilePage() {
     const { userName, profileSection } = useParams();
     const currentSection = profileSection || '';
-    
+    const [showConfirmationBox, setShowConfirmationBox] = useState(false);
+    useEffect(() => {
+        console.log("showConfirmationBox", showConfirmationBox);
+    }, [showConfirmationBox]);
+
+    const logOut = () => {
+        setShowConfirmationBox(false);
+        window.location.href = "/";
+    };
+
+    const cancelLogOut = () => {
+        setShowConfirmationBox(false);
+    };
+
     return (
         <div className="profile-page">
-            <Header/>
+            <Header />
+            {showConfirmationBox && (
+                <ConfirmationBox 
+                    content={"Do you want to log out?"} 
+                    confirm={logOut} 
+                    cancel={cancelLogOut} 
+                />
+            )}
             <div className="main-profile">
                 <div className="profile-nav">
                     <NavLink to={`/user/${userName}/profile/profileInfo`} className='profile-section'>
@@ -30,15 +50,18 @@ export default function ProfilePage() {
                         <i className="fa-solid fa-gear"></i>
                         <p>Settings</p>
                     </NavLink>
-                    <div className='profile-section'>
+                    <div className='profile-section' onClick={() => {
+                        console.log("Logout");
+                        setShowConfirmationBox(true)
+                    }}>
                         <i className="fa-solid fa-right-from-bracket"></i>
                         <p>Logout</p>
                     </div>
                 </div>
                 <div className="nav-section">
-                    <ProfileSectionNavigator profileSection={currentSection}/>
+                    <ProfileSectionNavigator profileSection={currentSection} />
                 </div>
             </div>
         </div>
-    )
+    );
 }
