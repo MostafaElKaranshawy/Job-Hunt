ef04c43e1260da47bcbc7f7a2750b5d5fdd554bc
Mostafa Elkaranshawy <mostafaelkaranshawy0@gmail.com>
2024-12-08 21:46:55 +0200

    SI-44 Editing Skills css and add good UX

--- frontend/src/components/skills/skills.jsx
+++ frontend/src/components/skills/skills.jsx
@@ -2,71 +2,103 @@ import React, { useEffect, useState } from "react";
 import "./skills.css";
 import { getUserSkills, editUserSkills } from "../../services/userProfileService";
 import { fetchSkills } from "../../services/APIsService";
+
 export default function Skills({username}) {
     const [filteredSkills, setFilteredSkills] = useState([]);
     const [skills, setSkills] = useState([]);
     const [search, setSearch] = useState("");
     const [isEditable, setIsEditable] = useState(false); // State to manage edit mode
     const [skillsLimit, setSkillsLimit] = useState(false)
+
     useEffect(() => {
+
         if(username){
+
             getUserSkills(username).then((skills) => {
+
                 setSkills(skills);
             });
         }
     }, [username]);
 
     useEffect(() => {
+
         if (search.trim() === "") {
+
             setFilteredSkills([]);
-        } else {
+        }
+        else {
+
             handleSearch();
         }
     }, [search]);
+
     const handleSearch = async () => {
+
         const searchedSkills = await fetchSkills(search.toLowerCase())
         console.log(searchedSkills)
-        if(search)
+
+        if (search)
             setFilteredSkills(searchedSkills.filter(skill => !skills.includes(skill)));
     }
-    // Handle toggling skills in the list
+    
     const addSkill = (skill) =>{
-        if(!isEditable)return
+
+        if (!isEditable)return
+
         if (skills.some((s) => s === skill)) {
-        } else {
+        }
+        else {
+
             if(skills.length >= 20){
+
                 setSkillsLimit(true)
                 return
             }
+
             setSkills([...skills, skill]);
             setFilteredSkills(filteredSkills.filter((s)=> s != skill))
         }
     }
+
     const removeSkill = (skill)=>{
-        if(!isEditable)return
+
+        if (!isEditable)return
+
         if (skills.some((s) => s === skill)) {
+
             setSkills(skills.filter((s) => s !== skill));
+
+            if (!filteredSkills.find((s) => s === skill))
+                setFilteredSkills([skill, ...filteredSkills]);
         }
+
         if(skills.length <= 10)
             setSkillsLimit(false)
     }
 
-    // Handle the edit button click to toggle edit mode
     const handleEditClick = () => {
+
         setIsEditable(!isEditable);
-    };
-    const handleSaveSkills = async ()=>{
-        handleCancel();
-        await editUserSkills(username, skills)
     }
+
+    const handleSaveSkills = async ()=>{
+
+        await editUserSkills(username, skills)
+        handleCancel();
+    }
+
     const handleCancel = async ()=>{
+
         setIsEditable(false)
         setSkillsLimit(false)
         setFilteredSkills([])
         setSearch("")
+
         const skillsData = await getUserSkills(username)
         setSkills(skillsData)
     }
+
     return (
         <div className="skills">
             <div className="section-edit" onClick={handleEditClick}>
@@ -78,9 +110,15 @@ export default function Skills({username}) {
                     <div
                         className="skill user-skill"
                         key={index}
-                        onClick={() => removeSkill(skill)}
                     >
                         {skill}
+                        {
+                            isEditable &&
+                            <span className="remove-skill-button"
+                                onClick={() => removeSkill(skill)}
+                            >x</span>
+                        }
+
                     </div>
                 ))}
             </div>
