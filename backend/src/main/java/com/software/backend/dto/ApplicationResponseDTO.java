a79b3d5e800f4e4a49b4472797f960bc718fae58
Nouran-Omnia <karemomnia399@icloud.com>
2025-01-01 00:42:07 +0200

    Displaying jobs to which applicant applied

---
+++ backend/src/main/java/com/software/backend/dto/ApplicationResponseDTO.java
@@ -0,0 +1,9 @@
+package com.software.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class ApplicationResponseDTO {
+    private String fieldName;
+    private String responseData;
+}
---
+++ backend/src/main/java/com/software/backend/mapper/JobApplicationMapper.java
@@ -0,0 +1,22 @@
+package com.software.backend.mapper;
+
+import com.software.backend.dto.ApplicantApplicationsResponseDto;
+import com.software.backend.dto.ApplicationResponseDTO;
+import com.software.backend.entity.ApplicationResponse;
+import com.software.backend.entity.JobApplication;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface JobApplicationMapper {
+
+    @Mapping(source = "job.company.name", target = "companyName")
+    @Mapping(source = "job.company.location", target = "companyAddress")
+    @Mapping(source = "job.company.website", target = "companyWebsite")
+    @Mapping(source = "applicationResponsesList", target = "responses")
+    ApplicantApplicationsResponseDto toApplicantApplicationsResponseDto(JobApplication application);
+
+    @Mapping(source = "field.label", target = "fieldName")
+    ApplicationResponseDTO toApplicationResponseDTO(ApplicationResponse response);
+}
+
