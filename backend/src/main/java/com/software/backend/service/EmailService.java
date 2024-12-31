60a6b6d1b2a7ff8f43ff76f9c0fdbe0d85d60c4d
Yomna Yasser <yomnay888@gmail.com>
2024-12-31 10:50:00 +0200

    SI-14 "having functions for sending emails when application is sent, applcant ges accepted or rejected, but needs ore details about jo or application in the body."

--- backend/src/main/java/com/software/backend/service/EmailService.java
+++ backend/src/main/java/com/software/backend/service/EmailService.java
@@ -34,4 +34,38 @@ public class EmailService {
         System.out.println("Email sent");
 
     }
+
+    public void sendApplicationSubmitConfirmationEmail(String to){
+        SimpleMailMessage message = new SimpleMailMessage();
+        String subject = "JOB_HUNT Application Submission Confirmation";
+        String body = "Your application has been submitted successfully. You will be notified once the company has reviewed your application.";
+        //need to add application details or job details , maybe a link to the application or job
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+
+    public void sendApplicationAcceptanceEmail(String to){
+        SimpleMailMessage message = new SimpleMailMessage();
+        String subject = "JOB_HUNT Application Acceptance";
+        String body = "Congratulations! Your application has been accepted. You will be contacted by the company soon.";
+        //need to add job details , maybe a link to the job
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+
+    public void sendApplicationRejectionEmail(String to){
+        SimpleMailMessage message = new SimpleMailMessage();
+        String subject = "JOB_HUNT Application Rejection";
+        String body = "We regret to inform you that your application has been rejected. Please keep applying to other jobs.";
+        //need to add job details , maybe a link to the job
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+
 }
